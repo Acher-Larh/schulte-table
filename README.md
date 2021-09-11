cef7e2df5e0e51fced642f32dcb2c0b5d5a6daf0
Acher-Larh <achdslayer@gmail.com>
2021-09-11 12:16:21 +0200

    built most of the functionality and added future steps on the tasks list.

--- README.md
+++ README.md
@@ -10,6 +10,24 @@ https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Ft
 There can only be 1 table active at a time -> Right now if you click more than once on the "begin" button it creates another table right under the old one. What we want is to first send an alert for the user to chose wether restart the table or to cancel the action. 
 
 TASKS LIST 
+
+* MARKUP 
+- [ ] make the navbar
+- [ ] make the footer
+
+* FUNCTIONALITY
 - [x] add the complete cell functionality -> change the "state" of a cell once it has been "clicked".
 
-- [ ] "There can only be 1 table active at a time.
+- [x] "There can only be 1 table active at a time.
+- [x] Congratulate de user if he succesfully cleared the table
+- [ ] in the alert give the score and whether or not it's a record. 
+- [x] remove the table after giving the score.
+- [x] make it so that the counter doesn't go higher than the table size.
+- [ ] make the navbar responsible so that it becomes a hamburger button when the screen shrunks.
+
+* STYLING
+- [ ] style the schulte table
+- [ ] make the animations for when a cell is clicked("correct" and "wrong" animations).
+- [ ] style the navbar
+- [ ] style the body: the title and buttons.
+- [ ] style the footer.
--- app.js
+++ app.js
@@ -4,15 +4,26 @@ const startButton = document.querySelector(".start-button");
 // Event Listeners
 startButton.addEventListener("click", makeTable);
 
+// Logical Variables
+let activeTable = false;
+const tableSize = 25;
+
 // Functions
 function makeTable(e) {
-  e.preventDefault();
+  e.preventDefault();  
+  if(activeTable != false) {
+    return;
+  }
+
+  activeTable = true;
+
   // get the reference for the body
-  var body = document.getElementsByTagName("body")[0];
+  // var body = document.getElementsByTagName("body")[0];
+  const container = document.querySelector(".schulte-container");
 
   // selects the <table> element and creates a <tbody> element
-  var tbl = document.querySelector(".schulte-table");
-  var tblBody = document.createElement("tbody");
+  const tbl = document.createElement("table");
+  const tblBody = document.createElement("tbody");
 
   // creating the cells content: numbers from 0 to 24(we will add 1 to that array element so that it goes from 1 to 25)
   const numbers = [...Array(25).keys()];
@@ -54,21 +65,50 @@ function makeTable(e) {
   // Adding a class to the table
   tbl.classList.add("schulte-table");
   // We append the <table> into the <body>
-  body.appendChild(tbl);
+  container.appendChild(tbl);
   // We set the border attribute of the tbl;
   tbl.setAttribute("border", "1");
 
   // We call the function that will triger each time a cell is "clicked".
   completeCell();
+
   
 }
 
-// This function will add the elements cells that have been "clicked" and will put them into an array.
+// This function will add the elements cells that have been "clicked" and will put their innerHtml into an array.
 function completeCell() {
-  const completedCells = [];
+
+  const clearedCells = [];
   document.querySelectorAll(".schulte-table td").
   forEach(e=>e.addEventListener("click", function (){
-    e.classList.add("completedCell");
-    completedCells.push(e.innerHTML);
+  if(!e.classList.contains("completed-cell") && !e.classList.contains("wrong-cell")) {
+      e.classList.add("completed-cell");
+      clearedCells.push(e.innerHTML);
+      isTblClear();
+  }else {
+    e.classList.add("wrong-cell");
+    console.log(e)
+  }
+    console.log(clearedCells.length);
+    console.log(tableSize);
+    
+    // This "if" will check if the table has been cleared. if it's true then it will congrat the user and remove the table, else it will prevent the user form creating a new table.
+    function isTblClear(){
+      if (clearedCells.length === tableSize || clearedCells.length > tableSize) {
+        alert("Well done! it took you x seconds");
+        activeTable = false;
+        removeTable();
+      }else {
+        activeTable = true;
+      }
+    }
+    
   }));
+  
+}
+
+function removeTable() {
+  const oldTable = document.querySelector(".schulte-table");
+  oldTable.parentNode.removeChild(oldTable);
+  
 }
